b120503db39a7d802aad4936930fba27b1470429
CGaulier <clemence.gaulier@hetic.net>
2019-01-30 18:04:01 +0100

    doctstring add

---
+++ data/__init__.py
@@ -0,0 +1,53 @@
+
+
+
+
+def weekDay (date):
+    '''
+    détermine le jour de la semaine
+    
+    arguments :
+    date -- date du csv
+
+    return : 
+    jour de la semaine (int)
+    '''
+    pass
+    
+
+def titleSeparation (title):
+    '''
+    Sépare le titre en tags
+    
+    arguments :
+    title -- titre de la video
+
+    return:
+    tableau de tags
+    '''
+    pass
+
+def dataPreprocess (dataFrame):
+    '''
+    rajoute colonne score et date dans dataframe principal
+    
+    arguments :
+    dataframe -- dataframe de données
+
+    return :
+    dataframe avec 2 colonnes en plus.
+    '''
+    pass
+
+def catIdentification (category, categoryJson):
+    '''
+    Détermine le code de la catégorie via le json
+    
+    arguments :
+    category --categorie de la video en string
+    categoryJson --fichier json
+
+    return:
+    code en int
+    '''
+    pass
---
+++ learning/__init__.py
@@ -0,0 +1,50 @@
+
+def csvImport (csv):
+    '''
+    Import le csv
+    
+    arguments :
+    csv --path du csv
+
+    return :
+    dataframe
+    '''
+    pass
+
+def jsonImport (json):
+    '''
+    Import le json
+    
+    arguments :
+    csv --path du json
+
+    return :
+    dataframe
+    '''
+    pass
+
+def titleScore (tags, titleData):
+    '''
+    determine un score en fonction des tags réccupérés.
+    
+    arguments :
+    tags -- tableau de tags provenant du titre
+    titleData -- données des tags du csv
+
+    return :
+    Un score
+    '''
+    pass
+
+def youtubeTrends (userInput, dataInput):
+    '''
+    Machine learning : prédiction si une video sera trend
+    
+    arguments :
+    userInput -- tableau de données entré par l'utilisateur
+    dataInput -- tebaleau de donnée du csv
+
+    return :
+    prédiction
+    '''
+    pass
