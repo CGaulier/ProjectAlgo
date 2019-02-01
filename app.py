8cdc76df8c3fb8298211bc7e379a3f32fb8c6adb
BOCQUILLON pierre <pierre.bocquillon@hetic.net>
2019-02-01 15:32:18 +0100

    home route is finaly working

--- app.py
+++ app.py
@@ -2,13 +2,15 @@ import pandas as pd
 import numpy as np
 import scipy as sp
 import sklearn
-import flask
 import functools
 from flask import (Blueprint, flash, g, redirect, render_template, request, session, url_for)
 from werkzeug.security import check_password_hash, generate_password_hash
+from flask import Flask
+
+app = Flask(__name__)
 
 bp = Blueprint('auth', __name__, url_prefix='/auth')
 
-@bp.route('/home', methods=('GET', 'POST'))
-def home():
-    return render_template('webInterface/home.html')
+@app.route('/')
+def hello_world():
+    return render_template('home.html')
