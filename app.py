2af0e08f7bbf83396c3146614842ec030040f3c9
BOCQUILLON pierre <pierre.bocquillon@hetic.net>
2019-02-01 15:08:52 +0100

    home route

--- app.py
+++ app.py
@@ -3,6 +3,11 @@ import numpy as np
 import scipy as sp
 import sklearn
 import flask
+import functools
+from flask import (Blueprint, flash, g, redirect, render_template, request, session, url_for)
+from werkzeug.security import check_password_hash, generate_password_hash
+
+bp = Blueprint('auth', __name__, url_prefix='/auth')
 
 @bp.route('/home', methods=('GET', 'POST'))
 def home():
